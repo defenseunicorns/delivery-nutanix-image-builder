80765aefe073e374be9c193df5ee13d111804d2c
Jacob May <jacob.may@defenseunicorns.com>
2024-01-23 17:51:24 -0500

    Postgres changed GPG key locations

--- packer/scripts/postgres-install.sh
+++ packer/scripts/postgres-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-wget https://apt.postgresql.org/pub/repos/yum/RPM-GPG-KEY-PGDG
+wget https://apt.postgresql.org/pub/repos/yum/keys/PGDG-RPM-GPG-KEY-RHEL
 sudo rpm --import RPM-GPG-KEY-PGDG
 sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo dnf -qy module disable postgresql
