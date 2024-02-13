8df723e58e10b832c03eff88d98aea122121a34e
Jacob May <jacob.may@defenseunicorns.com>
2024-02-13 14:20:32 -0500

    add rsync to dependency install list

--- packer/scripts/install-deps.sh
+++ packer/scripts/install-deps.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install dependencies and cli tools needed by other packer scripts and NDB service
 dnf update -y && yum upgrade -y
-dnf install -y ansible unzip iptables nftables wget lvm2* zip lsof network-scripts
+dnf install -y ansible unzip iptables nftables wget lvm2* zip lsof rsync network-scripts
 
 # Ensure that ansible collections needed are installed 
 ansible-galaxy collection install community.general
