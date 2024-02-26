3ad59375e73f1c479f04fca131c72624a8a7649a
jacobbmay <134300709+jacobbmay@users.noreply.github.com>
2024-02-26 16:33:50 -0500

    Update postgres image build (#3)
    * Add network-scripts package to installed dependencies
    
    * separate postgres version from build tag
    
    * update ansible role version
    
    * Postgres changed GPG key locations
    
    * forgot t change key name
    
    * add uefi boot option
    
    * add rsync to dependency install list

--- packer/scripts/postgres-install.sh
+++ packer/scripts/postgres-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-wget https://apt.postgresql.org/pub/repos/yum/RPM-GPG-KEY-PGDG
-sudo rpm --import RPM-GPG-KEY-PGDG
+wget https://apt.postgresql.org/pub/repos/yum/keys/PGDG-RPM-GPG-KEY-RHEL
+sudo rpm --import PGDG-RPM-GPG-KEY-RHEL
 sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo dnf -qy module disable postgresql
 sudo dnf install -y postgresql${POSTGRES_VERSION}-server
