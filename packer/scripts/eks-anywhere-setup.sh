c28f3c763a6cfc7e1ec026a047c859ba06215605
Jacob May <jacob.may@defenseunicorns.com>
2024-10-03 14:56:17 +0000

    forgot to save a file

--- packer/scripts/eks-anywhere-setup.sh
+++ packer/scripts/eks-anywhere-setup.sh
@@ -18,7 +18,16 @@ sudo yum groupinstall -y 'Development Tools'
 brew install gcc
 
 # Install eks-anywhere and deps needed
-brew install aws/tap/eks-anywhere
+curl "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" \
+    --silent --location \
+    | tar xz -C /tmp
+sudo install -m 0755 /tmp/eksctl /usr/local/bin/eksctl
+RELEASE_VERSION=${ANYWHERE_VERSION}
+EKS_ANYWHERE_TARBALL_URL=$(curl https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml --silent --location | yq ".spec.releases[] | select(.version==\"$RELEASE_VERSION\").eksABinary.$(uname -s | tr A-Z a-z).uri")
+curl $EKS_ANYWHERE_TARBALL_URL \
+    --silent --location \
+    | tar xz ./eksctl-anywhere
+sudo install -m 0755 ./eksctl-anywhere /usr/local/bin/eksctl-anywhere
 brew install yq
 
 # Install tfk8s which can be used to help convert kubernetes manifests into HCL
